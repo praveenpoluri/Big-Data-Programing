9ba01090a835c0c1652f9825f62fe8c139d1f66a
praveenpoluri <42703022+praveenpoluri@users.noreply.github.com>
2020-04-05 20:50:53 -0500

    Spark icp2

--- spark-icp1/WordCount_Latest/WordCount_Latest/src/main/scala/InvertedIndex.scala
+++ spark-icp1/WordCount_Latest/WordCount_Latest/src/main/scala/InvertedIndex.scala
@@ -2,7 +2,6 @@ import org.apache.spark._
 
 object InvertedIndex {
   def main(args: Array[String]) {
-
     System.setProperty("hadoop.home.dir","C:\\spark\\hadoop\\bin" )
     //val inputFile = args(0)
     //val outputFile = args(1)
@@ -12,25 +11,15 @@ object InvertedIndex {
     // Load our input data.
     //val input =  sc.textFile(inputFile)
     val input = sc.textFile("input\\sort.txt")
-
     val sorted = input.map(line => line.split(",")).map(x=>(x(0)+'-'+x(1),x(3))).partitionBy(new HashPartitioner(4))
-
     for(x<-sorted){
       print(x)
     }
-
     val sorted1= sorted.sortBy(_._2).groupByKey().mapValues(x=> List(x)).collect()
-
-
-
     for(w<-sorted1)
     {
       println(w)
     }
-
-
     //sorted1.saveAsTextFile("sorted")
-
-
   }
 }
